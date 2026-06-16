f553a4b95d1e3b9134417e9d9d706e2921e8019b
Graham Williamson <graham@williamsonsinc.id.au>
2026-06-16 23:15:20 +1000

    fix(zsh): use file completion for eza aliases

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -29,6 +29,33 @@ alias lta1='eza -lTag --level=1 --icons'
 alias lta2='eza -lTag --level=2 --icons'
 alias lta3='eza -lTag --level=3 --icons'
 
+## Make eza-backed aliases use eza completion.
+#if (( $+commands[eza] && $+functions[compdef] )); then
+#  compdef _eza l ls ll la
+#  compdef _eza lt lt1 lt2 lt3
+#  compdef _eza lta lta1 lta2 lta3
+#fi
+
+
+# File/path completion for eza-backed ls aliases.
+if (( $+functions[compdef] )); then
+  compdef _files l
+  compdef _files ls
+  compdef _files ll
+  compdef _files la
+
+  compdef _files lt
+  compdef _files lt1
+  compdef _files lt2
+  compdef _files lt3
+
+  compdef _files lta
+  compdef _files lta1
+  compdef _files lta2
+  compdef _files lta3
+fi
+
+
 # Python
 alias py='python3'
 alias pip='python3 -m pip'
